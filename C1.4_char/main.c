573951712dde8daf9832def0f9e0430367607fde
Kordyss99 <cforday@hotmail.com>
2026-01-02 06:30:40 +0300

    C1.4_char

---
+++ C1.4_char/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //char bellekte 1 byte yer kaplar ve 1 alanı vardır daha fazla alan için ilerleyen derslerde görecegiz
+    char isim1='C';
+    char isim2='F';
+    char isim3='O';
+    char isim4='R';
+    char isim5='D';
+    char isim6='A';
+    char isim7='Y';
+
+
+    printf("%c %c %c %c %c %c %c",isim1,isim2,isim3,isim4,isim5,isim6,isim7);
+    return 0;
+}
