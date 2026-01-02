a36718d09fa35d92cc7c551706d90a3329ad8c66
Kordyss99 <cforday@hotmail.com>
2026-01-02 06:46:44 +0300

    C1.5-printf-scanf

---
+++ C1.5-printf-scanf/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi;
+
+    printf("Lutfen 1 sayi giriniz: ");
+    scanf("%d",&sayi);
+
+    printf("Girdiginiz sayi : %d",sayi);
+    return 0;
+}
