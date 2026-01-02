a1f1c17dee2fee63785412dac7430a066216aff3
Kordyss99 <cforday@hotmail.com>
2026-01-02 06:53:27 +0300

    C1.5.2-printf-scanf

---
+++ C1.5.2-printf-scanf/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char karakter;
+
+    printf("Lutfen bir karakter giriniz: ");
+    scanf("%c",&karakter);
+
+    printf("Girdiginiz karakter: %c",karakter);
+    return 0;
+}
