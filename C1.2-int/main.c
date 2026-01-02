f465abec88fb4b797be0e070dc4731794ac58e8f
Kordyss99 <cforday@hotmail.com>
2026-01-02 06:29:50 +0300

    C1.2-int

---
+++ C1.2-int/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //2 farkı gösterim şekli vardır
+
+    //1
+    int s=20;
+    //2
+    int s2;
+    s2=20;
+
+    //yazarken ise
+     printf("%d",s);
+
+    //eğer 2 adet veri girilecekse
+
+    printf("\n%d %d",s,s2);
+
+
+     /*
+     printf("Hello world!\n");
+     */
+    return 0;
+}
