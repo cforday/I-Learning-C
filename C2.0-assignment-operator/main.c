4f71c6631024a6b136b0aaa7a7f8b713397da907
Kordyss99 <cforday@hotmail.com>
2026-01-03 23:36:37 +0300

    C2.0-assigment-operator

---
+++ C2.0-assignment-operator/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    = eşittir
+    += Toplayarak atama
+    -= Çıkararak atama
+    *= Çarparak atama
+    /= Bölerek atama
+    %= mod alarak atama
+    */
+
+    int x = 25; //assignment Operator
+
+    x+=1; // x=x+3;
+    //x-=1; //x=x-1
+    //x*=2; //x=x*2;
+    //x/=2; //x=x/2;
+    //x%=5; //x=x%2;
+
+
+    printf("%d",x);
+    return 0;
+}
