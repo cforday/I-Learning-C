baa5867f2612008f3ad98ffc539effd6d3ac8720
Kordyss99 <cforday@hotmail.com>
2026-01-04 22:12:44 +0300

    C2.3-if-else-and-else-if

---
+++ C2.3-if-else-and-else-if/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 25 , y = 35;
+
+    if(x>y)
+    {
+        printf("X Y'den buyuktur");
+    }
+    else
+        {
+            printf("X Y'den kucuktur");
+        }
+    return 0;
+}
