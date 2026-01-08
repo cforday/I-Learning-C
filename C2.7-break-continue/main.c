328b434e532b49b9ae53ca194d24b9a41a01007e
Kordyss99 <cforday@hotmail.com>
2026-01-08 22:15:50 +0300

    C2.7-break-continue

---
+++ C2.7-break-continue/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        if(i==5)
+        {
+            break;
+        }
+        printf("%d\n",i);
+    }
+    return 0;
+}
