7f9b72f575566bae84c38d90459557df8c2ca23f
cforday <cforday@hotmail.com>
2026-01-18 23:13:17 +0300

    C3.0-math.h

---
+++ C3.0-math.h/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+
+    // ceil , floor , sqrt , pow , abs
+    printf("%f\n",ceil(3.4));
+    printf("%f\n",floor(3.4));
+    printf("%f\n",sqrt(25));
+    printf("%f\n",pow(3,3));
+    printf("%d",abs(-30));
+
+
+
+    return 0;
+}
